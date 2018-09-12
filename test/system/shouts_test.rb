0113e834625205100d039a2e89899be55aca4da2
James <james.randolph2016@gmail.com>
2018-09-12 07:13:41 -0400

    scaffold SHout

---
+++ test/system/shouts_test.rb
@@ -0,0 +1,9 @@
+require "application_system_test_case"
+
+class ShoutsTest < ApplicationSystemTestCase
+  # test "visiting the index" do
+  #   visit shouts_url
+  #
+  #   assert_selector "h1", text: "Shout"
+  # end
+end
