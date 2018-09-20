b6cf59da0cba910cb01521287dcc61937093bbd3
James <james.randolph2016@gmail.com>
2018-09-19 21:39:04 -0400

    comment partial

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180920002621) do
+ActiveRecord::Schema.define(version: 20180920002939) do
 
   create_table "comments", force: :cascade do |t|
     t.string "name"
     t.text "comment"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "shout_id"
   end
 
   create_table "shouts", force: :cascade do |t|
